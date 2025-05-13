224d40f31fb94bb27b0d643054f16af514523d14
Marlon Cueva <marlon.cueva@talma.com.ec>
2025-05-13 18:22:44 -0500

    sincronizacion

--- Base/base_datos.py
+++ Base/base_datos.py
@@ -1,10 +1,9 @@
 import sqlite3
 
-db_path = "Base/ciudades.db"
-conn = sqlite3.connect(db_path)
+conn = sqlite3.connect("ciudades.db")
 cursor = conn.cursor()
 
-
+# Creamos Tabla
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS ciudades (
     CODIGO_CIUDAD TEXT PRIMARY KEY,
@@ -14,24 +13,27 @@ CREATE TABLE IF NOT EXISTS ciudades (
 );
 """)
 
-cursor.execute('''
-INSERT INTO ciudades (CODIGO_CIUDAD, CIUDAD, REGION, ALTURA)
-VALUES 
-('UIO', 'QUITO', 'SIERRA', 2850),
-('CUE', 'CUENCA', 'SIERRA', 2550),
-('LOH', 'LOJA', 'SIERRA', 2530),
-('AMB', 'AMBATO', 'SIERRA', 2500),
-('TEN', 'PUYO', 'ORIENTE', 950),
-('ZAM', 'ZAMORA', 'ORIENTE', 920),
-('SCY', 'SAN CRISTOBAL', 'COSTA', 730),
-('OCC', 'COCA', 'ORIENTE', 300),
-('NLO', 'NUEVA LOJA', 'ORIENTE', 297),
-('GPS', 'BALTRA', 'COSTA', 100),
-('ETR', 'SANTA ROSA', 'COSTA', 10),
-('MEC', 'MANTA', 'COSTA', 6),
-('GYE', 'GUAYAQUIL', 'COSTA', 4)
-''')
+# Insertamos datos en la tabla
+ciudad_region = [
+    ('UIO', 'QUITO', 'SIERRA', 2850),
+    ('CUE', 'CUENCA', 'SIERRA', 2550),
+    ('LOH', 'LOJA', 'SIERRA', 2530),
+    ('AMB', 'AMBATO', 'SIERRA', 2500),
+    ('TEN', 'PUYO', 'ORIENTE', 950),
+    ('ZAM', 'ZAMORA', 'ORIENTE', 920),
+    ('SCY', 'SAN CRISTOBAL', 'COSTA', 730),
+    ('OCC', 'COCA', 'ORIENTE', 300),
+    ('NLO', 'NUEVA LOJA', 'ORIENTE', 297),
+    ('GPS', 'BALTRA', 'COSTA', 100),
+    ('ETR', 'SANTA ROSA', 'COSTA', 10),
+    ('MEC', 'MANTA', 'COSTA', 6),
+    ('GYE', 'GUAYAQUIL', 'COSTA', 4)
+]
 
+cursor.executemany('''
+INSERT OR REPLACE INTO ciudades (CODIGO_CIUDAD, CIUDAD, REGION, ALTURA) 
+VALUES (?, ?, ?, ?)
+''', ciudad_region)
 
 conn.commit()
 conn.close()
