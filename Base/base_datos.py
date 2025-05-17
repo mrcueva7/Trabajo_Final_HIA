6363df3c5c23d9d7aea4430d5b4f0d3ba14354ca
Marlon Cueva <marlon.cueva@talma.com.ec>
2025-05-17 09:20:40 -0500

    sincronizacion

--- Base/base_datos.py
+++ Base/base_datos.py
@@ -6,7 +6,8 @@ cursor = conn.cursor()
 # Creamos Tabla
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS ciudades (
-    CODIGO_CIUDAD TEXT PRIMARY KEY,
+    id PRIMARY KEY,
+    CODIGO_CIUDAD TEXT NOY NULL,
     CIUDAD TEXT NOT NULL,
     REGION TEXT NOT NULL,
     ALTURA TEXT NOT NULL
@@ -15,24 +16,24 @@ CREATE TABLE IF NOT EXISTS ciudades (
 
 # Insertamos datos en la tabla
 ciudad_region = [
-    ('UIO', 'QUITO', 'SIERRA', 2850),
-    ('CUE', 'CUENCA', 'SIERRA', 2550),
-    ('LOH', 'LOJA', 'SIERRA', 2530),
-    ('AMB', 'AMBATO', 'SIERRA', 2500),
-    ('TEN', 'PUYO', 'ORIENTE', 950),
-    ('ZAM', 'ZAMORA', 'ORIENTE', 920),
-    ('SCY', 'SAN CRISTOBAL', 'COSTA', 730),
-    ('OCC', 'COCA', 'ORIENTE', 300),
-    ('NLO', 'NUEVA LOJA', 'ORIENTE', 297),
-    ('GPS', 'BALTRA', 'COSTA', 100),
-    ('ETR', 'SANTA ROSA', 'COSTA', 10),
-    ('MEC', 'MANTA', 'COSTA', 6),
-    ('GYE', 'GUAYAQUIL', 'COSTA', 4)
+    (1, 'UIO', 'QUITO', 'SIERRA', 2850),
+    (2, 'CUE', 'CUENCA', 'SIERRA', 2550),
+    (3, 'LOH', 'LOJA', 'SIERRA', 2530),
+    (4, 'AMB', 'AMBATO', 'SIERRA', 2500),
+    (5, 'TEN', 'PUYO', 'ORIENTE', 950),
+    (6, 'ZAM', 'ZAMORA', 'ORIENTE', 920),
+    (7, 'SCY', 'SAN CRISTOBAL', 'COSTA', 730),
+    (8, 'OCC', 'COCA', 'ORIENTE', 300),
+    (9, 'NLO', 'NUEVA LOJA', 'ORIENTE', 297),
+    (10, 'GPS', 'BALTRA', 'COSTA', 100),
+    (11, 'ETR', 'SANTA ROSA', 'COSTA', 10),
+    (12, 'MEC', 'MANTA', 'COSTA', 6),
+    (13, 'GYE', 'GUAYAQUIL', 'COSTA', 4)
 ]
 
 cursor.executemany('''
-INSERT OR REPLACE INTO ciudades (CODIGO_CIUDAD, CIUDAD, REGION, ALTURA) 
-VALUES (?, ?, ?, ?)
+INSERT OR REPLACE INTO ciudades (id, CODIGO_CIUDAD, CIUDAD, REGION, ALTURA) 
+VALUES (?, ?, ?, ?, ?)
 ''', ciudad_region)
 
 conn.commit()
